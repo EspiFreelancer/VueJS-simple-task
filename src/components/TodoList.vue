f49427877ffda37fee4b2abad0eca7c0529c4517
EspiFreelancer <espifreelancer@gmail.com>
2020-11-27 20:59:56 -0300

    Create the form to add tasks

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+	<form id="app" @submit.prevent="createTask">
+		<label class="label" for="task">Nueva tarea: </label>
+		<input type="text" v-model="newTask" id="task">
+		<!-- {{ newTask }} -->
+		<input type="submit" value="Crear tarea">
+	</form>
+</template>
+
+<script>
+	export default {
+		data: () => ({
+			newTask: "",
+			tasks: [] 
+		}),
+		methods: {
+			createTask() {
+				let task = {
+					text: this.newTask, 
+					completed: false
+				};
+				this.tasks.push(task);
+				this.newTask = "";
+				console.log("Method executed")
+				console.log(this.tasks)
+			}
+		}
+	};
+</script>
+
+<style>
+</style>
