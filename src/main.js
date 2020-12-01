af604f9cd5fcf5b96330b50fe5a403dd9d32ad08
EspiFreelancer <espifreelancer@gmail.com>
2020-11-30 23:42:30 -0300

    Add Vuex and VeeValidate

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
+import * as VeeValidate from 'vee-validate'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
