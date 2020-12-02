2d42b88a45506f5f3fa4a13f9af1866079939788
EspiFreelancer <espifreelancer@gmail.com>
2020-12-01 23:35:40 -0300

    Create the Store

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,48 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		items: [
+		{
+			name:"Retirar envió",
+			done: true
+		},
+		{
+			name:"Calificar producto",
+			done: false
+		},
+		{
+			name:"Avisar a Fred",
+			done: false
+		}
+		]
+	},
+	mutations: {
+		addItem(state, item) {
+			state.items.push(item)
+		},
+		editItem(state, { item, name = item.name, done = item.done }) {
+			item.name = name;
+			item.name = done;
+		},
+		removeItem(state, item) {
+			state.items.splice(state.items.indexOf(item), 1);
+		}
+	},
+	actions: {
+		addItem({ commit }, item) {
+			commit("addItem", {
+				name: item,
+				done: false
+			})
+		},
+		editItem({ commit }, { item, name }) {
+			commit("editItem", { item, name });
+		},
+		toggleItem({ commit }, item) {
+			commit("editItem", { item, done: !item.done })
+		}
+	},
+	modules: {
+	}
 })
